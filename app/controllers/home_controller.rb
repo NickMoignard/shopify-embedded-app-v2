69a19cf7c643259a27c9af4834891c0d0b9c2bc9
NickMoignard <nicklmoignard@gmail.com>
2019-05-24 11:17:54 +1000

    added customer csv export

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,24 @@
 
 class HomeController < AuthenticatedController
   def index
-    @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
-    @webhooks = ShopifyAPI::Webhook.find(:all)
+    shopify_shop = ShopifyAPI::Shop.current
+    @shop = Shop.find_by(:shopify_domain => shopify_shop.domain)
+    @customers = @shop.shopify_customers
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data  @customers.to_csv, :filename => "#{Date.today}-Customers.csv" }
+    end
+  end
+
+  def export
+    shopify_shop = ShopifyAPI::Shop.current
+    @shop = Shop.find_by(:shopify_domain => shopify_shop.domain)
+    @customers = @shop.shopify_customers
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data  @customers.to_csv, :filename => "#{Date.today}-Customers.csv" }
+    end
   end
 end
--- app/models/shopify_customer.rb
+++ app/models/shopify_customer.rb
@@ -1,3 +1,17 @@
 class ShopifyCustomer < ApplicationRecord
   belongs_to :shop
+
+  def self.to_csv
+    # TODO automatically determine headers with Model.first.attributes.keys
+    #   add csv extension to Models with csv export
+    attributes =  ["id", "shopify_id", "email", "first_name", "last_name", "orders_count", "total_spent", "last_order_id", "verified_email", "phone", "tags", "currency", "created_at", "updated_at", "shop_id"]
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      all.each do |customer|
+        csv << attributes.map{ |attr| customer.send(attr) }
+      end
+    end
+  end
 end
